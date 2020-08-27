89cbe0e453bc7f96f14818c49ca3d5d1c291c0c8
Olayinka Ibrahim <olayinka.ibrahim@etranzact.com>
2020-08-27 01:57:42 +0100

    added a youtube video

--- src/components/YoutubeVideo.js
+++ src/components/YoutubeVideo.js
@@ -4,14 +4,14 @@ const YoutubeVideo = () => {
     return (
         <Fragment>
             <div className="row p-4">
-                <div className="col-md-6">
+                <div className="col-md-6 mt-2">
                     <ReactPlayer 
                     url='https://www.youtube.com/watch?v=ZSesneKDTJ4'
                     controls={true} 
                     width="100%"
                     />
                 </div>
-                <div className="col-md-6">
+                <div className="col-md-6 mt-2">
                     <ReactPlayer 
                     url='https://www.youtube.com/watch?v=21n5N1UxGaU'
                     controls={true} 
